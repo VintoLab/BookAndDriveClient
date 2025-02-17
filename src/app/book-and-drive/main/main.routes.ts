0af5abf24c5ad3bf4be31695cdd9f06bf3ab79d3
RomanVintoniak <romanvintoniak03@gmail.com>
2025-02-17 11:53:05 +0200

    Fix routing bug

--- src/app/book-and-drive/main/main.routes.ts
+++ src/app/book-and-drive/main/main.routes.ts
@@ -3,12 +3,10 @@ import { HomeComponent } from "./home/home.component";
 import { CarListComponent } from "./car-list/car-list.component";
 import { OrderCreationComponent } from "./order-creation/order-creation.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
-import { NoPageComponent } from "../../shared/components/no-page/no-page.component";
 
 export const MAIN_ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'car-list', component: CarListComponent },
   { path: 'order-creation', component: OrderCreationComponent },
-  { path: 'profile', component: UserProfileComponent },
-  { path: '**', component: NoPageComponent }
+  { path: 'profile', component: UserProfileComponent }
 ]
