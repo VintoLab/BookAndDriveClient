f9e2853950921ee7b4400da0df9c96395faccb4d
RomanVintoniak <romanvintoniak03@gmail.com>
2025-02-17 21:28:56 +0200

    Add login form

--- src/app/book-and-drive/auth/login/login.component.ts
+++ src/app/book-and-drive/auth/login/login.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginModel } from '../models/login.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
 
+  form: FormGroup
+  isPasswordHidden = true;
+
+  constructor() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  onLogin() {
+    const inputtedData: LoginModel = this.form.value;
+    console.log(inputtedData);
+    // authService call
+  }
 }
