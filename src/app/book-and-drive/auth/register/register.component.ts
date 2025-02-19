f247d205688058f1104f414d8027d12c4db47fc9
RomanVintoniak <romanvintoniak03@gmail.com>
2025-02-19 17:51:16 +0200

    Add a registration form

--- src/app/book-and-drive/auth/register/register.component.ts
+++ src/app/book-and-drive/auth/register/register.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { NgxMaskDirective, provideNgxMask} from 'ngx-mask'
+import { RegisterModel } from '../models/register.model';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    NgxMaskDirective,
+  ],
+  providers: [
+    provideNgxMask()
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  fb = inject(FormBuilder);
 
+  form: FormGroup
+  phoneNumberInputPrefix: string = '+380';
+  isPasswordHidden = true;
+
+  constructor() {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onRegister() {
+    const inputtedData: RegisterModel = this.form.value;
+    inputtedData.phoneNumber = this.processInputtedPhoneNumber(inputtedData.phoneNumber);
+    console.log(inputtedData);
+  }
+
+  private processInputtedPhoneNumber(phoneNumber: string): string {
+    return this.phoneNumberInputPrefix + phoneNumber;
+  }
 }
