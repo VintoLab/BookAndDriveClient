3268daa23c2836c83ae1ff10fe60571ca2a91fcd
RomanVintoniak <romanvintoniak03@gmail.com>
2025-02-21 01:19:43 +0200

    Add route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,24 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './book-and-drive/main/main.component';
 import { AdminComponent } from './book-and-drive/admin/admin.component';
 import { NoPageComponent } from './shared/components/no-page/no-page.component';
+import { authGuard } from './core/guards/auth.guard';
+import { adminGuard } from './core/guards/admin.guard';
 
 export const routes: Routes = [
-  { path: '', component: MainComponent, loadChildren: () => import('./book-and-drive/main/main.routes').then(mod => mod.MAIN_ROUTES)},
-  { path: 'admin', component: AdminComponent, loadChildren: () => import('./book-and-drive/admin/admin.routes').then(mod => mod.ADMIN_ROUTES) },
-  { path: 'auth', loadChildren: () => import('./book-and-drive/auth/auth.routes').then(mod => mod.AUTH_ROUTES) },
+  {
+    path: '',
+    component: MainComponent,
+    loadChildren: () => import('./book-and-drive/main/main.routes').then(mod => mod.MAIN_ROUTES)
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [authGuard, adminGuard],
+    loadChildren: () => import('./book-and-drive/admin/admin.routes').then(mod => mod.ADMIN_ROUTES)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./book-and-drive/auth/auth.routes').then(mod => mod.AUTH_ROUTES)
+  },
   { path: '**', component: NoPageComponent }
 ];
--- src/app/book-and-drive/main/main.routes.ts
+++ src/app/book-and-drive/main/main.routes.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from "./home/home.component";
 import { CarListComponent } from "./car-list/car-list.component";
 import { OrderCreationComponent } from "./order-creation/order-creation.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { authGuard } from "../../core/guards/auth.guard";
 
 export const MAIN_ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'car-list', component: CarListComponent },
-  { path: 'order-creation', component: OrderCreationComponent },
-  { path: 'profile', component: UserProfileComponent }
+  { path: 'order-creation', component: OrderCreationComponent, canActivate: [authGuard] },
+  { path: 'profile', component: UserProfileComponent, canActivate: [authGuard] }
 ]
