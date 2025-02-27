aed05cf10fac63bd466213f77b2f1ae1ebc4b3f1
RomanVintoniak <romanvintoniak03@gmail.com>
2025-02-27 22:38:29 +0200

    Add advantages component

--- src/app/book-and-drive/main/home/home.component.ts
+++ src/app/book-and-drive/main/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { PhotoSliderComponent } from "./photo-slider/photo-slider.component";
 import { MatButtonModule } from '@angular/material/button';
+import { AdvantagesComponent } from './advantages/advantages.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     PhotoSliderComponent,
-    MatButtonModule
+    AdvantagesComponent,
+    MatButtonModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
