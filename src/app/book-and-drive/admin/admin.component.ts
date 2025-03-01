ca178302e17faef766860e93d6c2d125d3475b2f
RomanVintoniak <romanvintoniak03@gmail.com>
2025-03-01 20:44:17 +0200

    Add layout of admin component

--- src/app/book-and-drive/admin/admin.component.ts
+++ src/app/book-and-drive/admin/admin.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { LocalStorageService } from '../../core/services/local-storage.service';
+import { JwtService } from '../../core/services/jwt.service';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatListModule,
+    RouterLink
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  localStorageService = inject(LocalStorageService);
+  jwtService = inject(JwtService);
+  router = inject(Router);
 
+  token: string | null;
+
+  constructor() {
+    this.token = this.localStorageService.getToken();
+  }
+
+  logout() {
+    this.localStorageService.clear();
+    this.token = null;
+    this.router.navigate(['']);
+  }
 }
