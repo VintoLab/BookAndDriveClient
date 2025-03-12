2f721c3c0c8f204a1d4c7d111000e6b97fa92059
RomanVintoniak <romanvintoniak03@gmail.com>
2025-03-13 01:36:20 +0200

    Add cars displaying

--- src/app/book-and-drive/main/car-list/car-list.component.ts
+++ src/app/book-and-drive/main/car-list/car-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatChipSelectionChange, MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { CarType } from '../../../core/enums/car-type.enum';
 import { CarTransmission } from '../../../core/enums/car-transmission.enum';
 import { GetCarsQueryParams } from '../../../core/models/get-cars-query-params.model';
 import { CommonModule } from '@angular/common';
+import { Car } from '../../admin/models/car/car.model';
+import { CarsService } from '../../../shared/services/cars.service';
 
 @Component({
   selector: 'app-car-list',
@@ -12,12 +16,16 @@ import { CommonModule } from '@angular/common';
   imports: [
     MatChipsModule,
     MatIconModule,
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule
   ],
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.scss'
 })
-export class CarListComponent {
+export class CarListComponent implements OnInit {
+  carsService = inject(CarsService);
+  
   carType = CarType;
   carTransmission = CarTransmission;
 
@@ -27,20 +35,37 @@ export class CarListComponent {
     type: ''
   }
 
+  cars: Car[] = [];
+
+  ngOnInit(): void {
+    this.refetchData();
+  }
+
   onCarTransmissionChange($event: MatChipSelectionChange) {
     if ($event.selected) {
       this.queryParams.transmission = $event.source.value;
+      this.refetchData();
     } else {
       this.queryParams.transmission = '';
+      this.refetchData();
     }
   }
 
   onCarTypeChange($event: MatChipSelectionChange) {
     if ($event.selected) {
       this.queryParams.type = $event.source.value;
+      this.refetchData();
     } else {
       this.queryParams.type = '';
+      this.refetchData();
     }
+  }
+
+  private refetchData() {
+    this.carsService.getCars(this.queryParams).subscribe({
+      next: (data) => this.cars = data,
+      error: (error) => console.log(error.error)
+    });
   }
 }
 
